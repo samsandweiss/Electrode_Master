3991084a069a918e32cd4b8aa67759ec07f128de
Sam Sandweiss <sam.wacko.man@gmail.com>
2013-11-05 16:53:18 -0800

    SS_Alpha_9
    Big Update
    Character controller is finished. Jump/left/right
    Added level design. started making cubes for floors
    and many other things

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,112 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+	//bools for triggering doors and energy
 	public bool hasEnergy = false;
 	public bool atTheDoor = false;
-	public float movementSpeed = 1.0f;
+	
+	//values for character jump and move speed 
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	
 	public Rigidbody door;
 	public Light doorLight;
 	public Color holdingEnergy = Color.yellow;
 	public TextMesh successText;
-	private Vector3 movement = new Vector3 (0.0f, 0.0f, 0.0f);
-	
+
 	private float chargeValue = 1.000f;
 	private float chargeSpeed = 5.000f;
+	private Vector3 moveDirection = Vector3.zero;
 	
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		successText.gameObject.renderer.enabled = false;
 	}
 	
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update ()
+	{
 		
-		//Debug.Log(gameObject.transform.rotation.);
-		
-		if (Input.GetAxisRaw ("Horizontal") < 0) {
-			//moves the player to the left with smooth movement
-			
-			rigidbody.AddForce(Vector3.left*20);
-			
-			
-		//	movement = Vector3.left * movementSpeed * Time.deltaTime;
-		//	gameObject.transform.Translate (movement);
+		CharacterController controller = GetComponent<CharacterController> ();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("Jump"))
+				moveDirection.y = jumpSpeed; 
 		}
 		
-		if (Input.GetAxisRaw ("Horizontal") > 0) {
-			//moves the player to the left with smooth movement
-			
-			rigidbody.AddForce(Vector3.right*20);
-			
-		//	movement = Vector3.right * movementSpeed * Time.deltaTime;
-		//	gameObject.transform.Translate (movement);
-
-		}
-		
-		if (Input.GetAxisRaw ("Vertical") < 0) {
-			//moves the player to the left with smooth movement
-			movement = Vector3.back * movementSpeed * Time.deltaTime;
-			gameObject.transform.Translate (movement);
-		}
-		
-		if (Input.GetAxisRaw ("Vertical") > 0) {
-			//moves the player to the left with smooth movement
-			movement = Vector3.forward * movementSpeed * Time.deltaTime;
-			gameObject.transform.Translate (movement);
-		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+ 
 		
 		if ((Input.GetKeyDown (KeyCode.F) && atTheDoor)) {
-			door.GetComponent<Door>().Open();
-			Debug.Log("Door should have opened");
-		}
-		
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			rigidbody.AddForce(Vector3.up*100);
-		}
-		
+			door.GetComponent<Door> ().Open ();
+			Debug.Log ("Door should have opened");
+		}	
 	}
 	
-	void OnTriggerEnter(Collider otherCollider) {
+	void OnTriggerEnter (Collider otherCollider)
+	{
 		
-		if (otherCollider.gameObject.name.Contains("TeslaCoil")) {
-			otherCollider.gameObject.GetComponent<TeslaCoil>().ShowMessage();
+		if (otherCollider.gameObject.name.Contains ("TeslaCoil")) {
+			otherCollider.gameObject.GetComponent<TeslaCoil> ().ShowMessage ();
 			hasEnergy = true;
 			gameObject.renderer.material.color = holdingEnergy;
 		}
 		
-		if (otherCollider.gameObject.name.Contains("Door")) {
+		if (otherCollider.gameObject.name.Contains ("Door")) {
 			if (hasEnergy) {
-				otherCollider.gameObject.GetComponent<Door>().ShowUnlockedMessage();
+				otherCollider.gameObject.GetComponent<Door> ().ShowUnlockedMessage ();
 				atTheDoor = true;
 			} else {
-				otherCollider.gameObject.GetComponent<Door>().ShowLockedMessage();
+				otherCollider.gameObject.GetComponent<Door> ().ShowLockedMessage ();
 			}
 		}
 	}
 	
-	void OnTriggerStay(Collider otherCollider) {
-		if (otherCollider.gameObject.name.Contains("TeslaCoil")) {
-		charge();
+	void OnTriggerStay (Collider otherCollider)
+	{
+		if (otherCollider.gameObject.name.Contains ("TeslaCoil")) {
+			charge ();
 		}
 	}
-		
 	
-	void OnTriggerExit(Collider otherCollider) {
-		if (otherCollider.gameObject.name.Contains("Door")) {
-			otherCollider.gameObject.GetComponent<Door>().HideMessage();
+	void OnTriggerExit (Collider otherCollider)
+	{
+		if (otherCollider.gameObject.name.Contains ("Door")) {
+			otherCollider.gameObject.GetComponent<Door> ().HideMessage ();
 			atTheDoor = false;
 		}
 		
-		if (otherCollider.gameObject.name.Contains("TeslaCoil")) {
-			otherCollider.gameObject.GetComponent<TeslaCoil>().HideMessage();
+		if (otherCollider.gameObject.name.Contains ("TeslaCoil")) {
+			otherCollider.gameObject.GetComponent<TeslaCoil> ().HideMessage ();
 			atTheDoor = false;
 		}
 	}
-	void charge () {
-		chargeValue += Time.deltaTime*chargeSpeed;
-		Debug.Log(chargeValue);
-		Debug.Log(Time.deltaTime);
-}
+
+	void charge ()
+	{
+		chargeValue += Time.deltaTime * chargeSpeed;
+		Debug.Log (chargeValue);
+		Debug.Log (Time.deltaTime);
+	}
 }
