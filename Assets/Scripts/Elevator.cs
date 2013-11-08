3e49784af6ae8c20c4975d4c2ff4ba1f693545c5
Sam Sandweiss <sam.wacko.man@gmail.com>
2013-11-07 18:58:49 -0800

    SS_Alpha_15
    *NOTE THIS BUILD COULD BREAK GITHUB*
    pusedo code for elevator.
    2dToolkit has been added. may or may not transfer over.

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private int state= 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (state == 1) {
+			//move elevator down
+		}
+		if (state == 3) {
+			//move elevator up
+		}
+		
+			
+		
+	
+	}
+	
+	void onTriggerStay () {
+		//if elevator hits bottom
+		//then state ++ and set bool elevator bottom to true
+		
+		//if elevator hits top
+		//then state ++ elevator movement and se
+		
+		
+	
+	}
+	
+	void activate() {
+		state ++;
+			if (state > 3) {
+				state = 0;
+			}
+}
+}
