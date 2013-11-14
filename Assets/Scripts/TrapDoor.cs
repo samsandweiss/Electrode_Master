29427f615eb0f2b6f158fd58a7587443dc5214d6
Sam Sandweiss <sam.wacko.man@gmail.com>
2013-11-14 09:49:12 -0800

    SS_Alpha_24
    Elevator Code Implemented
    Trap Door Code Implemented

--- Assets/Scripts/TrapDoor.cs
+++ Assets/Scripts/TrapDoor.cs
@@ -5,10 +5,9 @@ public class TrapDoor : MonoBehaviour
 {
 	public Transform rotationCenter;
 	public int rotationSpeed = 50;
-	public bool upPosition;
-	public bool downPosition;
-	public bool movingForward;
 	public bool isActivated;
+	public int counter;
+	public int modCounter;
 	
 	// Use this for initialization
 	void Start ()
@@ -19,41 +18,56 @@ public class TrapDoor : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (movingForward) {
-			//moving forward code
-			if (rotationCenter.eulerAngles.z >= 360) {
-				isActivated = false;
-				//upPosition = true;
-			} else {
-				if (isActivated) {
-					rotationCenter.Rotate (Vector3.down, rotationSpeed * Time.deltaTime);
-					//upPosition = false;	
-				}
-			}
-		
-		} else {
-			//moving backward code
-			if (rotationCenter.eulerAngles.z <= 270) {
-				//downPosition = true;
-			} else {
-				if (isActivated) {
-					rotationCenter.Rotate (Vector3.up, rotationSpeed * Time.deltaTime);
-					//downPosition = false;	
-				}
-			}
+		if (modCounter == 0) {
+			//trap door at bottom or stopped
 		
 		}
 		
+		if (modCounter == 1) {
+			//trap door moving up
+			rotationCenter.Rotate (Vector3.down, rotationSpeed * Time.deltaTime);
+		}
+		
+		if (modCounter == 2) {
+			//trap door at top or stopped (preparing to move up)
+			
+		}
+		
+		if (modCounter == 3) {
+			//trap door moving down
+			rotationCenter.Rotate (Vector3.up, rotationSpeed * Time.deltaTime);
+		}
+			
+	}
+
+		
+		
+	void OnTriggerEnter (Collider otherCollider)
+	{
+		
+		if (otherCollider.gameObject.name.Contains ("TrapDoorBottom")) {
+			counter = 0;
+			modCounter = 0;
+		}
+
+			
+		if (otherCollider.gameObject.name.Contains ("TrapDoorTop")) {
+			counter = 2;
+			modCounter = 2;
+			
+	
+		}
+
+	
 		
 	}
 	
 	public void Activate ()
 	{
-		isActivated = true;
-		
-		if (movingForward)
-			movingForward = false;
-		else
-			movingForward = true;
+	
+		counter++;
+		modCounter = (counter % 4);
+		Debug.Log (modCounter);
+
 	}
 }
