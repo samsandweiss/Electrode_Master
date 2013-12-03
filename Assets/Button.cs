a63f40f3792f71cfe05c2f55055f67d7d9e82a0e
mcarlile <mckenzie.carlile@gmail.com>
2013-12-03 13:35:29 -0800

    MC_Alpha_44
    Reorganized the File

---
+++ Assets/Button.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnMouseOver ()
+	{
+		renderer.material.color -= new Color (0.1F, 0, 0) * Time.deltaTime;
+		Debug.Log ("mouseover");
+	}
+}
