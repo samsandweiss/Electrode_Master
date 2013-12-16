a9e2f1006be2c99c408d2a6804a24b18b1e0a73f
Sam Sandweiss <sam.wacko.man@gmail.com>
2013-12-15 16:30:14 -0800

    SS_Beta_4
    Added:
    Tesla Coil scripting for sound
    one tesla coil sound.
    
    Still Need to do: (if we have time)
    button sounds
    door sounds
    elevator sounds
    trap door sounds.

--- Assets/Scripts/TeslaCoil.cs
+++ Assets/Scripts/TeslaCoil.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class TeslaCoil : MonoBehaviour
 {
 	public ParticleEmitter emitter;
+	public AudioClip teslainactive;
+	public AudioClip teslaactive;
+
 	void Start ()
 	{
+		audio.clip = teslainactive;
+		audio.Play();
 	}
 	
 
@@ -20,6 +25,7 @@ public class TeslaCoil : MonoBehaviour
 			Debug.Log ("Tesla coil is being hit");
 			//otherCollider.GetComponent<LightningBolt> ().target = GameObject.transform;
 			emitter.active = true;
+			audio.clip = teslaactive;
 		}
 	}
 
@@ -27,6 +33,7 @@ public class TeslaCoil : MonoBehaviour
 	{
 		if (otherCollider.name.Contains ("Player")) {
 			emitter.active = false;
+			audio.clip = teslainactive;
 		}
 	}
 }
