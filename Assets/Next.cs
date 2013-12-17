dfc075e0ffad82a967f3541458e260091ccd9385
mcarlile <mckenzie.carlile@gmail.com>
2013-12-17 09:15:26 -0800

    MC_Beta_11
    Created Intro Scenes with control scheme.

---
+++ Assets/Next.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Next : MonoBehaviour
+{
+	public int levelToLoad;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		if (Input.GetButtonDown ("Jump")) {
+			Debug.Log ("Should load new level");
+			Application.LoadLevel (levelToLoad);
+		}
+	
+	}
+}
